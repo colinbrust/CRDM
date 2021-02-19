6bb97019c1ec11977b8f1137639484627a41bdce
colinbrust <colin.brust@gmail.com>
2021-02-19 10:51:01 -0700

    Added new normalization method to see how difference in normalization affect performance.

--- crdm/R/NormalizeData.R
+++ crdm/R/NormalizeData.R
@@ -2,7 +2,7 @@ library(magrittr)
 args <- commandArgs(trailingOnly=TRUE)
 
 # Stack rasters of same variable, normalize between -1 and 1, write out
-normalize <- function(variable, in_dir, out_dir) {
+normalize_across_domain <- function(variable, in_dir, out_dir) {
 
   f_list <- list.files(in_dir, pattern = variable, full.names = T) 
   
@@ -25,14 +25,34 @@ normalize <- function(variable, in_dir, out_dir) {
       )
     
   }
+} 
+
+norm_calc <- function(x) {
+  
+  (2*(x - min(x, na.rm = T))/(max(x, na.rm = T) - min(x, na.rm = T))) - 1
+  
+}
+
+normalize_across_pixel <- function(variable, in_dir, out_dir) {
+  
+  f_list <- list.files(in_dir, pattern = variable, full.names = T) 
+  
+  stk <-  f_list %>%
+    raster::stack() %>%
+    raster::calc(norm_calc) %>%
+    raster::as.list()
+  
+  out_names <- file.path(out_dir, basename(f_list))
+  
+  mapply(raster::writeRaster, x=stk, filename=out_names, overwrite = T)
 }  
 
 variables = c('pr', 'rmax', 'rmin', 'sm-rootzone', 'sm-surface', 'srad', 'tmmn', 
-              'tmmx', 'vpd', 'vs', 'fw', 'vapor', 'VOD') %>% paste0('.tif')
+              'tmmx', 'vpd', 'vs', 'fw', 'vapor', 'vod') %>% paste0('.tif')
 
 # variables = c('gpp', 'ET') %>% paste0('.tif')
 
 variables %>%
-  parallel::mclapply(normalize, in_dir = args[1], out_dir = args[2], mc.cores = 13)
+  parallel::mclapply(normalize_across_pixel, in_dir = args[1], out_dir = args[2], mc.cores = 13)
 
   

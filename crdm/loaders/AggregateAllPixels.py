eb2cd19af96349b7be15807b9a0582705539744d
colinbrust <colin.brust@gmail.com>
2021-03-08 10:56:06 -0700

    Small change to make AggregateAllPixels.py work with multiple lead times round 2.

--- crdm/loaders/AggregateAllPixels.py
+++ crdm/loaders/AggregateAllPixels.py
@@ -41,15 +41,18 @@ class AggregateAllPixles(Aggregate):
         # Add day of year for target image.
         target_doy = self.target_date.timetuple().tm_yday
         target_doy = (target_doy - 1) / (366 - 1)
+        target_doy_week = np.ones_like(weeklys[0]) * target_doy
         target_doy = np.ones_like(constants[0]) * target_doy
 
         # Add day of year for image guess date.
         guess_doy = self.guess_date.timetuple().tm_yday
         guess_doy = (guess_doy - 1) / (366 - 1)
+        guess_doy_week = np.ones_like(weeklys[0]) * guess_doy
         guess_doy = np.ones_like(constants[0]) * guess_doy
 
         day_diff = self._get_day_diff()
         day_diff = (day_diff - 7) / (84 - 7)
+        day_diff_week = np.ones_like(weeklys[0]) * day_diff
         day_diff = np.ones_like(constants[0]) * day_diff
 
         constants = np.concatenate((constants, target_doy[np.newaxis]))

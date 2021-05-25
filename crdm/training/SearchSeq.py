b72630b3d19a763ad42cb796d96103f414748010
colinbrust <colin.brust@gmail.com>
2021-05-25 16:28:18 -0600

    Added code for hyperparameter tuning of new model.

--- crdm/training/SearchSeq.py
+++ crdm/training/SearchSeq.py
@@ -1,57 +1,39 @@
 import argparse
 from crdm.models.SeqTest import Seq2Seq
-from crdm.models.SeqVanilla import Seq2Seq as vanilla
-from crdm.training.TrainModel import train_model
 from crdm.loaders.LSTMLoader import LSTMLoader
+from crdm.training.TrainModel import train_model
 from crdm.utils.ModelToMap import Mapper
-import numpy as np
 import os
 import torch
 from torch import nn
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 import pickle
-from sklearn.utils.class_weight import compute_class_weight
+import pprint
 
 
 def train_lstm(setup):
 
-    with open(os.path.join(setup['dirname'], 'shps.p'), 'rb') as f:
-        shps = pickle.load(f)
+    shps = setup['shps']
 
     # Make train and test set data loaders and add them to model setup
     train_loader = LSTMLoader(dirname=setup['dirname'], train=True, categorical=setup['categorical'],
-                              n_weeks=setup['n_weeks'], sample=150000, mx_lead=setup['mx_lead'])# sample=131072)
+                              n_weeks=setup['n_weeks'], sample=1500, even_sample=setup['even_sample'])
+
     test_loader = LSTMLoader(dirname=setup['dirname'], train=False, categorical=setup['categorical'],
-                             n_weeks=setup['n_weeks'], sample=150000, mx_lead=setup['mx_lead'])# sample=131072)
+                             n_weeks=setup['n_weeks'], sample=1500, even_sample=False)
+
     setup['train'] = DataLoader(dataset=train_loader, batch_size=setup['batch_size'], shuffle=True, drop_last=True)
     setup['test'] = DataLoader(dataset=test_loader, batch_size=setup['batch_size'], shuffle=True, drop_last=True)
 
-    if setup['model_type'] == 'vanilla':
-        print('Using vanilla model.')
-        setup['batch_first'] = True
-        model = vanilla(1, shps['train_x.dat'][1], setup['hidden_size'], setup['mx_lead'], setup['categorical'])
-    elif setup['model_type'] == 'attention':
-        print('Using simple attention.')
-        setup['batch_first'] = True
-        model = Seq2Seq(1, shps['train_x.dat'][1], setup['n_weeks'],
-                        setup['hidden_size'], setup['mx_lead'], categorical=setup['categorical'])
-    else:
-        raise ValueError("setup['model_type'] must be one of 'vanilla' or 'attention'.")
+    setup['batch_first'] = True
+    model = Seq2Seq(1, shps['train_x.dat'][1], setup['n_weeks'], setup['hidden_size'], setup['mx_lead'],
+                    categorical=setup['categorical'])
 
-    if setup['categorical']:
-        y = np.memmap(os.path.join(setup['dirname'], 'train_y.dat'), dtype='float32')
-        y = y*5
-        y = y.astype(np.int8)
-        class_weights = compute_class_weight('balanced', classes=np.unique(y), y=y)
-        print('Class Weights: {}'.format(class_weights))
-        weights = torch.Tensor(class_weights).type(
-            torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
-
-    criterion = nn.CrossEntropyLoss(weight=weights) if setup['categorical'] else nn.MSELoss()
+    criterion = nn.MSELoss()
     lr = 0.002
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, amsgrad=True)
-    scheduler = StepLR(optimizer, step_size=5, gamma=0.5, verbose=True)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, threshold=0.00001,  verbose=True)
 
     setup['model'] = model
     setup['criterion'] = criterion
@@ -64,78 +46,56 @@ def train_lstm(setup):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Drought Prediction Model')
-    parser.add_argument('-p', '--pick', type=str, help='Pickle File with the setup dictionary.')
-    # parser.add_argument('-t', '--out_classes', type=str, help='Directory with targets')
-    # parser.add_argument('-e', '--epochs', type=int, default=25, help='Number of epochs.')
-    # parser.add_argument('-dn', '--dirname', type=str, default=None,
-    #                     help='Directory with training data to use. If left blank, training data will be created.')
-    #
-    # parser.add_argument('--cat', dest='categorical', action='store_true', help='Treat targets as categorical')
-    # parser.add_argument('--no-cat', dest='categorical', action='store_false',
-    #                     help="Treat targets as continuous")
-    #
-    # parser.add_argument('--van', dest='vanilla', action='store_true', help='Use Vanilla Seq2Seq model')
-    # parser.add_argument('--no-van', dest='vanilla', action='store_false', help="Use Seq2Seq with attetion")
-    #
-    # parser.set_defaults(search=False)
-    #
+    parser.add_argument('-if', '--in_features', type=str, help='Directory containing training features.')
+    parser.add_argument('-c', '--out_classes', type=str, help='Directory containing target classes.')
+    parser.add_argument('-e', '--epochs', type=int, default=25, help='Number of epochs.')
+    parser.add_argument('-dn', '--dirname', type=str, default=None,
+                        help='Directory with training data to use. If left blank, training data will be created.')
+
+    parser.add_argument('--es', dest='even_sample', action='store_true', help='Sample training data classes evenly.')
+    parser.add_argument('--no-es', dest='even_sample', action='store_false', help='Sample training data normally')
     args = parser.parse_args()
-    #
-    # setup = {'in_features': args.in_features, 'out_classes': args.out_classes, 'epochs': args.epochs,
-    #          'early_stop': 10, 'categorical': args.categorical,
-    #          'pix_mask': '/mnt/e/PycharmProjects/DroughtCast/data/pix_mask.dat',
-    #          'model': 'vanilla' if args.vanilla else 'attention', 'dirname': args.dirname}
 
-    setup = {'in_features': args.in_features, 'out_classes': args.out_classes, 'epochs': args.epochs,
-             'early_stop': 10, 'categorical': args.categorical,
-             'pix_mask': '/mnt/e/PycharmProjects/DroughtCast/data/pix_mask.dat', 
-             'model_type': 'vanilla' if args.vanilla else 'attention', 'dirname': args.dirname}
-    # global, con
-    # global, cat
-    # pix, cat
-    # pix, con
-    # pix, attn
-    # pix, van
+    with open(os.path.join(args.dirname, 'shps.p'), 'rb') as f:
+        shps = pickle.load(f)
 
-    with open(args.pick, 'rb') as f:
-        setups = pickle.load(f)
-        
-    for setup in setups:
+    setup = {
+        'in_features': args.in_features,
+        'out_classes': args.out_classes,
+        'epochs': args.epochs,
+        'n_weeks': 30,
+        'mx_lead': 12,
+        'size': 1024,
+        'lead_time': None,
+        'early_stop': 10,
+        'categorical': False,
+        'pix_mask': '/mnt/e/PycharmProjects/DroughtCast/data/pix_mask.dat',
+        'model_type': 'vanilla',
+        'dirname': args.dirname,
+        'even_sample': args.even_sample,
+        'shps': shps
 
-        with open(os.path.join(setup['dirname'], 'shps.p'), 'rb') as f:
-            shps = pickle.load(f)
+    }
 
-        i = 0
-        while os.path.exists(os.path.join(setup['dirname'], 'metadata_{}.p'.format(i))):
-            i += 1
+    for batch in [128, 256, 512]:
 
-        setup['index'] = i
+        for hidden in [128, 256, 512]:
+            i = 0
+            while os.path.exists('model_{}'.format(i)):
+                i += 1
 
-    for batch in [64, 128, 256, 512]:
-        for hidden in [1, 64, 128, 256, 512]:
-            for history in [2, 15, 30]:
-                for lead in [2, 8, 12]:
-                    i = 0
-                    while os.path.exists(os.path.join(setup['dirname'], 'metadata_{}.p'.format(i))):
-                        i += 1
+            out_dir = 'model_{}'.format(i)
+            os.mkdir(out_dir)
+            setup['out_dir'] = out_dir
 
-                    setup['index'] = i
-                    setup['batch_size'] = batch
-                    setup['hidden_size'] = hidden
-                    setup['mx_lead'] = lead
-                    setup['n_weeks'] = history
+            setup['index'] = i
+            setup['batch_size'] = batch
+            setup['hidden_size'] = hidden
 
-                    with open(os.path.join(setup['dirname'], 'metadata_{}.p'.format(setup['index'])), 'wb') as f:
-                        pickle.dump(setup, f)
+            with open(os.path.join(setup['out_dir'], 'metadata.p'), 'wb') as f:
+                pickle.dump(setup, f)
 
-                    print(setup.keys())
-                    model = train_lstm(setup)
-                    out_dir = os.path.join(setup['dirname'], 'preds_{}'.format(setup['index']))
-                    mpr = Mapper(model, setup, args.in_features, args.out_classes, out_dir, shps, True, None, setup['categorical'])
-                    mpr.get_preds()
-
-        print(setup)
-        model = train_lstm(setup)
-        out_dir = os.path.join(setup['dirname'], 'preds_{}'.format(setup['index']))
-        mpr = Mapper(model, setup, setup['in_features'], setup['out_classes'], out_dir, shps, True, None, setup['categorical'])
-        mpr.get_preds()
+            pprint.pprint(setup)
+            model = train_lstm(setup)
+            mpr = Mapper(model, setup, args.in_features, args.out_classes, out_dir, shps, True, None, setup['categorical'])
+            mpr.get_preds()
--- crdm/training/TrainModel.py
+++ crdm/training/TrainModel.py
@@ -10,8 +10,7 @@ def train_model(setup):
     criterion = setup['criterion']
     scheduler = setup['scheduler']
     early_stop = setup['early_stop']
-    batch_first = setup['batch_first']
-    
+
     err_out = {}
 
     if torch.cuda.is_available():
@@ -24,7 +23,6 @@ def train_model(setup):
         total_loss = 0
         train_loss = []
         test_loss = []
-        lrs = []
 
         model.train()
 
@@ -32,9 +30,7 @@ def train_model(setup):
         for idx, item in enumerate(setup['train']):
             x, y = item
             x, y = x.squeeze(), y.squeeze()
-            if not batch_first:
-                print('asdf')
-                x = x.transpose(0, 1)
+
             # Zero out the optimizer's gradient buffer
             optimizer.zero_grad()
             # Make prediction with model
@@ -58,8 +54,7 @@ def train_model(setup):
 
         for idx, item in enumerate(setup['test']):
             x, y = item
-            if not batch_first:
-                x = x.transpose(0, 1)
+
             outputs = model(x)
             outputs = outputs.squeeze()
 
@@ -73,9 +68,8 @@ def train_model(setup):
             test_loss.append(loss.item())
 
         # Save out train and test set loss.
-        err_out[epoch] = {'train': train_loss, 'test': test_loss, 'lr': lrs}
-        # err_out[epoch] = {'train': sum(train_loss)/len(train_loss),
-        #                   'test': sum(test_loss)/len(test_loss)}
+        err_out[epoch] = {'train': sum(train_loss)/len(train_loss),
+                          'test': sum(test_loss)/len(test_loss)}
 
         with open(os.path.join(setup['out_dir'], 'err.p'), 'wb') as f:
             pickle.dump(err_out, f)
@@ -96,7 +90,7 @@ def train_model(setup):
         if no_improvements >= early_stop:
             break
 
-        scheduler.step()
+        scheduler.step(total_loss)
 
     del setup['scheduler']
     del setup['criterion']
